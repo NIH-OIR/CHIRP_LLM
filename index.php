c62705deedf01256fa322a79c2b096dcf4d67fa2
lilyfan <linpei.fan@nih.gov>
2024-08-16 17:22:50 -0400

    Move gemini_handler.js to index.php since azureuser cannot create a new file in the directory

--- index.php
+++ index.php
@@ -200,7 +200,40 @@ foreach(array_keys($models) as $m) {
         var user = <?php echo json_encode(isset($user) ? $user : null); ?>;
 
     </script>
-    <script type="module" src="gemini_handler.js"></script>
+    <script type="module">
+        import {
+            getGenerativeModel,
+            fileToGenerativePart,
+            updateUI,
+        } from "./geminiutils/shared.js";
+
+        async function run(prompt, files) {
+            const imageParts = await Promise.all(
+                [...files].map(fileToGenerativePart),
+            );
+
+            const model = await getGenerativeModel({
+                model: "gemini-1.5-flash",
+            });
+
+            return model.generateContentStream([...imageParts, prompt]);
+        }
+
+        window.getResultFromGemini = async function(prompt, files) {
+            // console.log("getResultFromGemini prompt: "+prompt);
+            const result = await run(prompt, files);
+            // console.log("getResultFromGemini result: "+result);
+            return result;
+        };
+
+        window.updateUIFromGemini = async function(resultEl, getResult, streaming) {
+            //console.log("updateUIFromGemini");
+            const result = await updateUI (resultEl, getResult, streaming);
+            //console.log("updateUIFromGemini result: "+result);
+            return result;
+        };
+
+    </script>
     <script src="script.v1.02.js"></script>
     <script>
         //document.addEventListener('DOMContentLoaded', function() {
